b2f15966295e0c1dfd6e64ea0b68a40a8409de00
aljorgevi <jormencar@yahoo.com>
2023-07-11 21:49:49 +0100

    update package.json, license & config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import dts from "vite-plugin-dts";
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
 	plugins: [react(), dts(), cssInjectedByJsPlugin()],
 	build: {
 		lib: {
-			entry: resolve(__dirname, "src/index.ts"),
-			name: "MyComponent",
-			fileName: "my-component",
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'ThreadCraft',
+			fileName: 'thread-craft'
 		},
 		rollupOptions: {
-			external: ["react"],
+			external: ['react'],
 			output: {
 				globals: {
-					react: "React",
-				},
-			},
-		},
-	},
-});
+					react: 'React'
+				}
+			}
+		}
+	}
+})
