206f7b99eedb17b4ac5aeff8683240cdc4998a5b
aljorgevi <jormencar@yahoo.com>
2023-07-22 11:01:28 +0100

    add Blog

--- src/MyComponent.stories.ts
+++ src/MyComponent.stories.ts
@@ -1,17 +1,20 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { MyComponent } from "./MyComponent";
+import { Blog } from "./Blog";
 
 const meta = {
-	title: "MyComponent",
-	component: MyComponent,
-} satisfies Meta<typeof MyComponent>;
+	title: "Blog",
+	component: Blog,
+} satisfies Meta<typeof Blog>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
 	args: {
-		title: "My Component",
+		title: "Blog Title",
+		description: "Blog Description",
+		image:
+			"https://cdn.shopify.com/s/files/1/0293/3928/7612/t/8/assets/ad27cd772c54--AlexB-BlogHeader.jpg?v=1683272580",
 	},
 };
