b5e64794177901da6d37a4266b3f5dd934b3ffb7
aljorgevi <jormencar@yahoo.com>
2023-07-22 11:01:36 +0100

    add Blog

---
+++ tests/Blog.cy.tsx
@@ -0,0 +1,17 @@
+import { Blog } from "../src/Blog";
+
+describe("<MyComponent />", () => {
+	it("renders", () => {
+		cy.mount(
+			<Blog
+				image="https://cdn.shopify.com/s/files/1/0293/3928/7612/t/8/assets/ad27cd772c54--AlexB-BlogHeader.jpg?v=1683272580"
+				title="My title"
+				description="My description"
+			>
+				<div>My component</div>
+			</Blog>
+		);
+
+		cy.findByText(/My component/i);
+	});
+});
